97e41122e50f33456b2e7646c89a918def64bce6
Sabrina Simeroth <Sabrina Simeroth>
2017-03-31 13:52:55 -0700

    Initial Commit

---
+++ vstda-api/VSTDA/VSTDA.API/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VSTDA.API.Models
+{
+	public class TodoItem
+	{
+		public int TodoItemId { get; set; }
+
+		public string task { get; set; }
+
+		public int priority { get; set; }
+	}
+}
